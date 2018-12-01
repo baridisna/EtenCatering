39fa0e3b40b84df32ead3e5efca7bf1a2e9f3183
baridisna <33018302+baridisna@users.noreply.github.com>
2018-12-01 19:50:34 +0700

    add model for product and user

--- application/models/model_product.php
+++ application/models/model_product.php
@@ -4,7 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class model_product extends CI_Model {
 	public function list_product()
 	{
-		
+		$query = $this->db->query('SELECT a.product_id, a.product_name, a.photo, MIN(b.price) as price FROM products a JOIN product_variant ON a.product_id = b.product_id GROUP BY product_id LIMIT 50');
+		return $query->result();	
+	}
+
+	public function product_detail($id_product)
+	{
+		$query = $this->db->query("SELECT product_id, product_name, product_description, photo FROM products WHERE product_id='$id_product'");
+		return $query->row();
+	}
+
+	public function product_variant($id_product)
+	{
+		$query = $this->db->query("SELECT * FROM product_variant WHERE product_id='$id_product'");
+		return $query->row();
 	}
 }
 ?>
